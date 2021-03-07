b955a8475b6183118d3f205ac7ad320948351731
10micky10 <10miguel94@gmail.com>
2021-03-06 22:13:45 -0800

    add logo fixes

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "./logo-condores.jpg";
 import "./Logo.css";
 
 class Logo extends Component {
+  //return <img src={logo} className="App-logo" alt="logo" />;
   render() {
     return <img src={logo} className="App-logo" alt="logo" />;
   }
